ab84dd70edb0b94357869d09323d8e41e252c5ce
Fouad <fouadezari@gmail.com>
2022-01-31 13:46:05 +0100

    doc(progress-bar): add readme to progress bar project

---
+++ 02-progress-steps/readme.md
@@ -0,0 +1,8 @@
+# Progress Steps 🏃🏻
+
+This project it's a progress bar. A lot of websites use it with forms, shopping to see the progression of the carts, with multilevels forms etc...
+
+We will create two buttons **'Next'** and **'Prev'**. with this we'll progress one by one until final step.
+Of course in the first step the button **Prev** must be disabled and for the last steps **Next** button 'll be it.
+
+![progress](../capture/progress-bar.png)
--- 02-progress-steps/script.js
+++ 02-progress-steps/script.js
@@ -6,35 +6,34 @@ const circles = document.querySelectorAll('.circle')
 let currentActive = 1
 
 next.addEventListener('click', () => {
-  currentActive ++
-  if(currentActive > circles.length) currentActive = circles.length
-  updateCircle()
+	currentActive++
+	if (currentActive > circles.length) currentActive = circles.length
+	updateCircle()
 })
 
 prev.addEventListener('click', () => {
-  currentActive --
-  if(currentActive < 1) currentActive = 1
-  updateCircle()
+	currentActive--
+	if (currentActive < 1) currentActive = 1
+	updateCircle()
 })
 
 function updateCircle() {
-  circles.forEach((circle, index) => {
-    (index < currentActive) 
-    ? circle.classList.add('active') 
-    : circle.classList.remove('active')
-  })
-  const actives = document.querySelectorAll('.active')
-  progress.style.width = ((actives.length-1)/(circles.length-1)) * 100 +'%'
-  // console.log(actives.length/circles.length *100)
-  // console.log((actives.length/circles.length) *100 )
+	circles.forEach((circle, index) => {
+		index < currentActive
+			? circle.classList.add('active')
+			: circle.classList.remove('active')
+	})
+	const actives = document.querySelectorAll('.active')
+	progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%'
+	// console.log(actives.length/circles.length *100)
+	// console.log((actives.length/circles.length) *100 )
 
-  if(currentActive === 1) {
-    prev.disabled = true
-  } else if (currentActive === circles.length){
-    next.disabled = true
-  }else {
-    prev.disabled = false
-    next.disabled = false
-  }
-
+	if (currentActive === 1) {
+		prev.disabled = true
+	} else if (currentActive === circles.length) {
+		next.disabled = true
+	} else {
+		prev.disabled = false
+		next.disabled = false
+	}
 }
