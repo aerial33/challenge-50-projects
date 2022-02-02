437b6b61c2f9752384e68e6278bd32341b6d8ba7
Fouad <fouadezari@gmail.com>
2022-02-02 20:10:18 +0100

    refactor: cleaner code with the linters

--- 02-progress-steps/script.js
+++ 02-progress-steps/script.js
@@ -4,36 +4,36 @@ const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
-
-next.addEventListener('click', () => {
-	currentActive++
-	if (currentActive > circles.length) currentActive = circles.length
-	updateCircle()
-})
-
-prev.addEventListener('click', () => {
-	currentActive--
-	if (currentActive < 1) currentActive = 1
-	updateCircle()
-})
-
 function updateCircle() {
 	circles.forEach((circle, index) => {
 		index < currentActive
 			? circle.classList.add('active')
 			: circle.classList.remove('active')
 	})
-	const actives = document.querySelectorAll('.active')
-	progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%'
-	// console.log(actives.length/circles.length *100)
-	// console.log((actives.length/circles.length) *100 )
+}
 
-	if (currentActive === 1) {
-		prev.disabled = true
-	} else if (currentActive === circles.length) {
-		next.disabled = true
-	} else {
-		prev.disabled = false
-		next.disabled = false
-	}
+next.addEventListener('click', () => {
+	currentActive += 1
+	if (currentActive > circles.length) currentActive = circles.length
+	updateCircle()
+})
+
+prev.addEventListener('click', () => {
+	currentActive -= 1
+	if (currentActive < 1) currentActive = 1
+	updateCircle()
+})
+
+const actives = document.querySelectorAll('.active')
+progress.style.width = `${((actives.length - 1) / (circles.length - 1)) * 100}%`
+// console.log(actives.length/circles.length *100)
+// console.log((actives.length/circles.length) *100 )
+
+if (currentActive === 1) {
+	prev.disabled = true
+} else if (currentActive === circles.length) {
+	next.disabled = true
+} else {
+	prev.disabled = false
+	next.disabled = false
 }
--- 05-blurry-loading/script.js
+++ 05-blurry-loading/script.js
@@ -1,18 +1,18 @@
 const bg = document.querySelector('.bg')
 const loadText = document.querySelector('.loading-text')
 
-let load = 0;
-
-let int = setInterval(blurring, 30)
+let load = 0
+const int = setInterval(blurring, 30)
 
 function blurring() {
-  load++
-  if (load > 99) clearInterval(int)
-  loadText.innerText = `${load}%`
-  loadText.style.opacity = scale(load, 0, 100, 1, 0)
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+	load += 1
+	if (load > 99) clearInterval(int)
+	loadText.innerText = `${load}%`
+	loadText.style.opacity = scale(load, 0, 100, 1, 0)
+	bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
+// eslint-disable-next-line arrow-body-style
 const scale = (number, inMin, inMax, outMin, outMax) => {
-  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+	return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin
 }
