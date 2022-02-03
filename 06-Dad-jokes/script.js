4b1396ca2906187c35e789da4729a826a581ea16
Fouad <fouadezari@gmail.com>
2022-02-03 15:31:39 +0100

    feat(Dad jokes): Add api deepl translator and display translate in french

--- 06-Dad-jokes/script.js
+++ 06-Dad-jokes/script.js
@@ -1,12 +1,24 @@
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
+const deepEl = document.getElementById('deepl')
 
 async function generateJoke() {
-	const config = { headers: { Accept: 'application/json' } }
-	const res = await fetch('https://icanhazdadjoke.com', config)
-	const data = await res.json()
-	jokeEl.innerHTML = data.joke
-	// console.log(data)
+	try {
+		const config = { headers: { Accept: 'application/json' } }
+		const resJoke = await fetch('https://icanhazdadjoke.com', config)
+		const JokeData = await resJoke.json()
+		const jokeText = JokeData.joke
+		jokeEl.innerHTML = jokeText
+		// console.log(data)
+		const resDeepl = await fetch(
+			`https://api-free.deepl.com/v2/translate?auth_key=[your_auth_key]&text=${jokeText}&target_lang=FR`
+		)
+		const DeeplData = await resDeepl.json()
+		deepEl.innerHTML = DeeplData.translations[0].text
+	} catch (error) {
+		console.error(error)
+		jokeEl.innerHTML = 'No Joke sorry !'
+	}
 }
 
 generateJoke()
